49de9c519a645b77ab4661425d2f787c88cd837d
VIGNESH007-DE <vigneshwaranethicalhacker@gmail.com>
2026-04-03 15:20:09 +0530

    Update vite-env.d.ts

--- --page-by-page-requirements-1-indexhtml-landing-page-html-structure---animated-hero-section-with-gradient-backgro/src/vite-env.d.ts
+++ --page-by-page-requirements-1-indexhtml-landing-page-html-structure---animated-hero-section-with-gradient-backgro/src/vite-env.d.ts
@@ -1,13 +1,14 @@
 /// <reference types="vite/client" />
 
-interface ImportMetaEnv {
-  readonly VITE_FIREBASE_API_KEY: string;
-  readonly VITE_FIREBASE_AUTH_DOMAIN: string;
-  readonly VITE_FIREBASE_PROJECT_ID: string;
-  readonly VITE_FIREBASE_STORAGE_BUCKET: string;
-  readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
-  readonly VITE_FIREBASE_APP_ID: string;
-}
+const firebaseConfig = {
+  apiKey: "AIzaSyBhSg8Z0mOxUISGY7Bf6Uw-zoWg6A50f_c",
+  authDomain: "medical-tracker-579a5.firebaseapp.com",
+  projectId: "medical-tracker-579a5",
+  storageBucket: "medical-tracker-579a5.firebasestorage.app",
+  messagingSenderId: "841507530920",
+  appId: "1:841507530920:web:fe60f9dfa2fc1c2cbc7a9d",
+  measurementId: "G-1H2KDGQHYF"
+};
 
 interface ImportMeta {
   readonly env: ImportMetaEnv;
